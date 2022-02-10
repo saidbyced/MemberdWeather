f94f0bb617699d080367708f761be8872b16910d
Chris Eadie <chris.eadie@outlook.com>
2022-02-10 14:44:56 +0000

    Initial Commit

---
+++ MemberdWeather/MemberdWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemberdWeatherApp.swift
+//  MemberdWeather
+//
+//  Created by Christopher Eadie on 10/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct MemberdWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
